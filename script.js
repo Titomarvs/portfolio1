c99b7348cbc796ecb60aab6f67ac4ef1d622078c
Marvic <mpajaganas2002@gmail.com>
2025-03-23 01:46:44 +0800

    animation

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+      }
+  });
+}, { threshold: 0.2 });
+
+document.querySelectorAll(".hidden").forEach((el) => observer.observe(el));
